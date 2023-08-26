f76887f59b20b2a7df2bec42970908d1b7558eb0
Mehboob-alam1 <ma8122576@gmail.com>
2023-08-26 12:45:38 +0000

    done reading notifications

--- app/src/main/java/com/mehboob/hunzarider/fragments/NotificationsFragment.java
+++ app/src/main/java/com/mehboob/hunzarider/fragments/NotificationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.work.Data;
 
 import android.view.LayoutInflater;
@@ -16,14 +17,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mehboob.hunzarider.R;
+import com.mehboob.hunzarider.adapters.NotificationAdapter;
 import com.mehboob.hunzarider.constants.Constants;
 import com.mehboob.hunzarider.databinding.FragmentNotificationsBinding;
+import com.mehboob.hunzarider.models.NotifFirebase;
+
+import java.util.ArrayList;
 
 
 public class NotificationsFragment extends Fragment {
 
 
- FragmentNotificationsBinding binding;
+   private FragmentNotificationsBinding binding;
+   private ArrayList<NotifFirebase> list;
+   private NotificationAdapter notificationAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,17 +38,11 @@ public class NotificationsFragment extends Fragment {
         // Inflate the layout for this fragment
 
 
-        binding=FragmentNotificationsBinding.inflate(inflater,container,false);
-
-
-
-
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        list= new ArrayList<>();
 
 
         fetchNotifications();
-
-
-
 
 
         return binding.getRoot();
@@ -49,22 +50,38 @@ public class NotificationsFragment extends Fragment {
 
     private void fetchNotifications() {
 
-        DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
         ref.child(Constants.RIDER).child("UserNotifications").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
+                if (snapshot.exists()) {
+                   binding.progresbar.setVisibility(View.GONE);
+                    for (DataSnapshot snap : snapshot.getChildren()) {
+
+                      NotifFirebase notifFirebase=  snap.getValue(NotifFirebase.class);
+                      list.add(notifFirebase);
+
+                    }
+                    notificationAdapter= new NotificationAdapter(requireContext(),list);
+                    binding.recyclerNotification.setAdapter(notificationAdapter);
+                    binding.recyclerNotification.setLayoutManager(new LinearLayoutManager(requireContext()));
 
 
-                }else{
+                } else {
+
+                    binding.progresbar.setVisibility(View.GONE);
+                    binding.noDataLayout.getRoot().setVisibility(View.VISIBLE);
+                    binding.recyclerNotification.setVisibility(View.GONE);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                binding.progresbar.setVisibility(View.GONE);
+                binding.noDataLayout.getRoot().setVisibility(View.VISIBLE);
+                binding.recyclerNotification.setVisibility(View.GONE);
             }
-        })
+        });
     }
 }
